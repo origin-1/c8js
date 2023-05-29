5f9c1b9b9a5610f6d430d9e5263dd2506df12659
Francesco Trotta <github@fasttime.org>
2023-05-30 00:32:38 +0200

    Switch to flat config

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import { createFlatConfig } from '@origin-1/eslint-config';
+import globals              from 'globals';
+
+export default createFlatConfig
+(
+    {
+        ignores: ['**/.*', 'coverage', 'test/fixtures', 'tmp'],
+    },
+    {
+        files:      ['**/*.cjs', '**/*.js'],
+        jsVersion:  2020,
+    },
+    {
+        files:              ['**/*.ts'],
+        tsVersion:          'latest',
+        languageOptions:    { parserOptions: { project: 'tsconfig.json' } },
+    },
+    {
+        languageOptions: { globals: { ...globals.node } },
+    },
+    {
+        files:      ['dev/*.js'],
+        jsVersion:  2022,
+    },
+    {
+        files:              ['test/*.spec.js'],
+        languageOptions:    { globals: { ...globals.mocha } },
+    },
+);
